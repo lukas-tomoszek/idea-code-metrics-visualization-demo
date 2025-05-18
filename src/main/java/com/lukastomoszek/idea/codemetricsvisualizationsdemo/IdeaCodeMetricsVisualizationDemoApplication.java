342d4e24a8a7e7e1cd27b890f3eb70f73843f22b
Lukáš Tomoszek <lukas.tomoszek@gmail.com>
2025-05-18 16:46:47 +0200

    Initial commit

---
+++ src/main/java/com/lukastomoszek/idea/codemetricsvisualizationsdemo/IdeaCodeMetricsVisualizationDemoApplication.java
@@ -0,0 +1,12 @@
+package com.lukastomoszek.idea.codemetricsvisualizationsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaCodeMetricsVisualizationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaCodeMetricsVisualizationDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/lukastomoszek/idea/codemetricsvisualizationsdemo/IdeaCodeMetricsVisualizationDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.lukastomoszek.idea.codemetricsvisualizationsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaCodeMetricsVisualizationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
